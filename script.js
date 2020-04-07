32b7b47410904fd7cf006ec6f3af95817597e3e8
teneons <teneons@gmail.com>
2020-04-07 14:38:09 +0300

    Change count year

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ let startVS = moment('2016-10-14T20:56')
 let nowDate = moment()
 
 
-let yaersVS = nowDate.diff(startVS, 'year')
 let monthsVS = nowDate.diff(startVS, 'month')
+    let yaersVS = (monthsVS / 12) + 0.1; //nowDate.diff(startVS, 'year')
 let daysVS = nowDate.diff(startVS, 'day')
 let hoursVS = nowDate.diff(startVS, 'hours')
 let minutesVS = nowDate.diff(startVS, 'minutes')
 let secondsVS = nowDate.diff(startVS, 'seconds')
 
-document.getElementById('yaersVS').innerHTML = `${yaersVS} years`;
+document.getElementById('yaersVS').innerHTML = `${yaersVS.toPrecision(2)} years`;
 document.getElementById('monthsVS').innerHTML = `${monthsVS} months`;
 document.getElementById('daysVS').innerHTML = `${daysVS} days`;
 document.getElementById('hoursVS').innerHTML = `${hoursVS} hours`;
