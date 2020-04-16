e2322e4502305e1720b0e637992f666a95b8337b
teneons <teneons@gmail.com>
2020-04-16 16:23:07 +0300

    update js file

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
-let startVS = moment('2016-10-14T20:56')
-let nowDate = moment()
+function outDate() {
+    const startVS = moment('2016-10-14T20:56')
+    let nowDate = moment(new Date())
 
-let monthsVS = nowDate.diff(startVS, 'month')
+    let monthsVS = nowDate.diff(startVS, 'month')
     let yaersVS = (monthsVS / 12) + 0.1; //nowDate.diff(startVS, 'year')
-let daysVS = nowDate.diff(startVS, 'day')
-let hoursVS = nowDate.diff(startVS, 'hours')
-let minutesVS = nowDate.diff(startVS, 'minutes')
-let secondsVS = nowDate.diff(startVS, 'seconds')
+    let daysVS = nowDate.diff(startVS, 'day')
+    let hoursVS = nowDate.diff(startVS, 'hours')
+    let minutesVS = nowDate.diff(startVS, 'minutes')
+    let secondsVS = nowDate.diff(startVS, 'seconds')
 
-document.getElementById('yaersVS').innerHTML = `${yaersVS.toPrecision(2)} years`;
-document.getElementById('monthsVS').innerHTML = `${monthsVS} months`;
-document.getElementById('daysVS').innerHTML = `${daysVS} days`;
-document.getElementById('hoursVS').innerHTML = `${hoursVS} hours`;
-document.getElementById('minutesVS').innerHTML = `${minutesVS} minutes`;
-document.getElementById('secondsVS').innerHTML = `${secondsVS} seconds`;
+    document.getElementById('yaersVS').innerHTML = `${yaersVS.toPrecision(2)} years`;
+    document.getElementById('monthsVS').innerHTML = `${monthsVS} months`;
+    document.getElementById('daysVS').innerHTML = `${daysVS} days`;
+    document.getElementById('hoursVS').innerHTML = `${hoursVS} hours`;
+    document.getElementById('minutesVS').innerHTML = `${minutesVS} minutes`;
+    document.getElementById('secondsVS').innerHTML = `${secondsVS} seconds`;
+}
+
+setInterval(outDate, 1000)
 
 
+
+//btn heart
 let heartVS = document.getElementById("heartVS");
 
-function easterEgg () {
+//easterEgg
+function easterEgg() {
     let clickCount = 0;
 
     const startEE =  () => {
