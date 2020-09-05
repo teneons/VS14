acdf86a6a87044660b5a3fb97f36e2bf386c2567
teneons <teneons@gmail.com>
2020-09-05 13:00:29 +0300

    change calculate years

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 function outDate() {
     const startVS = moment('2016-10-14T20:56')
-    let nowDate = moment(new Date())
+    let nowDate = moment()
 
     let monthsVS = nowDate.diff(startVS, 'month')
-    let yaersVS = (monthsVS / 12) + 0.1; //nowDate.diff(startVS, 'year')
+    let yaersVS = nowDate.diff(startVS, 'year')
+        let yearToMonth = yaersVS * 12
+        let differentMonth = monthsVS - yearToMonth
+        let rightYear = `${yaersVS}.${differentMonth}`
     let daysVS = nowDate.diff(startVS, 'day')
     let hoursVS = nowDate.diff(startVS, 'hours')
     let minutesVS = nowDate.diff(startVS, 'minutes')
     let secondsVS = nowDate.diff(startVS, 'seconds')
 
-    document.getElementById('yaersVS').innerHTML = `${yaersVS.toPrecision(2)} years`;
+    document.getElementById('yaersVS').innerHTML = `${rightYear} years`;
     document.getElementById('monthsVS').innerHTML = `${monthsVS} months`;
     document.getElementById('daysVS').innerHTML = `${daysVS} days`;
     document.getElementById('hoursVS').innerHTML = `${hoursVS} hours`;
@@ -18,8 +21,6 @@ function outDate() {
 }
 
 setInterval(outDate, 1000)
-
-
 
 //btn heart
 let heartVS = document.getElementById("heartVS");
